e35f17f259a8fa697402b7970b9c755b41b82633
Kanti R. Patel <demo-user@kantipatel.com>
2024-01-25 11:43:08 -0500

    added simple langchain jupyter notebooks

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import os
 import csv
 
+for key, value in os.environ.items():
+    print(f"{key}: {value}")
+    
+openai_api_key = os.getenv("OPENAI_API_KEY")
+print(openai_api_key)
+
 def app_setup():
+    
     cwd = os.getcwd()
     file_name = f"{cwd}/data/persons.csv"
 
