bec8e698e29e44bb0e4831df2f584d79fc36e606
Kanti R. Patel <demo-user@kantipatel.com>
2024-01-29 06:57:54 -0500

    updated chatbot module

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
 import os
 import csv
 
-for key, value in os.environ.items():
-    print(f"{key}: {value}")
-    
-openai_api_key = os.getenv("OPENAI_API_KEY")
-print(openai_api_key)
-
 def app_setup():
     
     cwd = os.getcwd()
     file_name = f"{cwd}/data/persons.csv"
-
-    with open(file_name, newline='') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            print(row)
-    
+    # TODO: 
 
 def main():
     app_setup()
